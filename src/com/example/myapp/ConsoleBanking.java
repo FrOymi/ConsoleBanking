d2ab9c35d343573903113fa827207511f696078f
FrOymi <vadimprudskyi@gmail.com>
2026-01-19 17:19:52 +0100

    Added switching between tabs

--- src/com/example/myapp/ConsoleBanking.java
+++ src/com/example/myapp/ConsoleBanking.java
@@ -5,6 +5,20 @@ import com.example.myapp.Interface;
 public class ConsoleBanking {
     // Test code
     static void main() {
-        Interface.showAuthorizationPage();
+        String rt;
+        while (true) {
+            rt = Interface.showAuthorizationPage();
+            switch (rt) {
+                case "1":
+                    if (Interface.showLogInPage()) {
+                        continue;
+                    }
+                    break;
+                case "2":
+                    // TODO: implement this later
+                case "3":
+                    System.exit(0);
+            }
+        }
     }
 }
--- src/com/example/myapp/Interface.java
+++ src/com/example/myapp/Interface.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Interface {
     static Scanner sc = new Scanner(System.in);
+    static String error = "";
     public static String showAuthorizationPage() {
         System.out.print("===============================\n" +
                 "Authorization in ConsoleBanking\n" +
@@ -12,40 +13,41 @@ public class Interface {
                 " Create a new account |    2   \n" +
                 " Exit                 |    3\n" +
                 "---------------------- --------\n" +
+                error +
                 "Input >>> ");
         String selectedButton = sc.nextLine();
         if (selectedButton.equals("1") || selectedButton.equals("2") || selectedButton.equals("3")) {
             return selectedButton;
         }
-        System.out.println("Invalid input");
+        error = "Invalid input\n";
         return showAuthorizationPage();
     }
 
-    public static void showLogInPage() {
-        String email;
-        String password;
-        System.out.print("===============================\n" +
-                "   ---LogIn ConsoleBanking---\n" +
-                "===============================\n" +
-                "Email:                 <<<\n" +
-                "Password:\n" +
-                "--------------- ----------------\n" +
-                "  1. Enter     |      2. Back\n" +
-                "--------------- ----------------\n" +
-                "Input >>> ");
-        String usrInput = sc.nextLine();
-        if (usrInput.equals("1")) {
-
-            // TODO: implement this later
-
-        } else if (usrInput.equals("2")) {
-
-            // TODO: implement this later
-
+    public static Boolean showLogInPage() {
+        String[] data = {"", ""};
+        for (int i = 0; i<=data.length; i++) {
+            System.out.print("===============================\n" +
+                    "   ---LogIn ConsoleBanking---\n" +
+                    "===============================\n" +
+                    "Email: " + data[0] + "\n" +
+                    "Password: " + data[1] + "\n" +
+                    "--------------- ----------------\n" +
+                    "  1. Enter     |      2. Back\n" +
+                    "--------------- ----------------\n" +
+                    "Input >>> ");
+            String usrInput = sc.nextLine();
+            if (usrInput.equals("1")) {
+                // TODO: implement this later
+            } else if (usrInput.equals("2")) {
+                break;
+            } else {
+                data[i] = usrInput;
+            }
         }
+        return true;
     }
 }
 
 class InputProcessing {
-
+    // TODO: implement this later
 }
